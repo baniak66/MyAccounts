1dc3312d0991ce14e392c3617e3ca65654fdb39e
Pawel Licznar <pawellicznar@gmail.com>
2016-03-19 00:39:46 +0100

    Add edit, update and destroy actions for evidences

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -5,6 +5,8 @@ class EvidencesController < ApplicationController
   expose(:contractor)
   expose(:decree) {Decree.new}
 
+  before_action :check_decrees, only: :destroy
+
   def show
   end
 
@@ -25,9 +27,33 @@ class EvidencesController < ApplicationController
     end
   end
 
+  def edit
+    contractor = Contractor.find(params[:contractor_id])
+  end
+
+  def update
+    if evidence.update(evidence_params)
+      redirect_to evidence, notice: 'Evidence changed.'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if evidence.destroy
+      redirect_to evidences_path, notice: 'Evidence deleted.'
+    end
+  end
+
   private
 
     def evidence_params
       params.require(:evidence).permit(:number, :madedate, :amount, :description, :contractor_id, :accept)
+    end
+
+    def check_decrees
+      unless evidence.decrees.empty?
+        redirect_to evidence_path, flash: {error: 'Evidence has decrees, so You can not delete it.'}
+      end
     end
 end
